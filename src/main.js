c9a321116f5c0abfdf6f422682a30c5c64cb3959
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-02-28 22:17:32 -0800

    button over

--- src/main.js
+++ src/main.js
@@ -5,23 +5,54 @@ const canvas = document.getElementById('canvas3d');
 
 // start the application and load the scene
 const spline = new Application(canvas);
-// https://prod.spline.design/KM1Kt9xSuyb7rHI5/scene.splinecode
+// 'https://prod.spline.design/HGAiXo5EKsHcv1mJ/scene.splinecode'
 
 spline
-	.load('https://prod.spline.design/KM1Kt9xSuyb7rHI5/scene.splinecode')
+	.load('https://prod.spline.design/6IPZMO5uk1wGCExP/scene.splinecode')
 	.then(() => {
-		const obj = spline.findObjectByName('Cube');
-		// or
-		// const obj = spline.findObjectById('7AF5EBC0-09BB-4720-B045-F478F8053AA4');
-        spline.addEventListener('mouseDown', (e) => {
-			// 检查是否点击了名为'Cube'的对象
-			if (e.target.name === 'Cube') {
-                console.log('点击了Cube');
-				// 旋转90度
-				// 注意：Spline使用的是弧度而不是度，所以需要将度转换为弧度
-				// Math.PI / 2 弧度等于90度
-				e.target.rotation.y += Math.PI / 2;
-			}});
+		const obj = spline.findObjectByName('Sphere');
 		console.log(obj); // Spline Object => { name: 'Cube', id: '7AF5EBC0-09BB-4720-B045-F478F8053AA4', position: {}, ... }
 
+		const obj2 = spline.findObjectById('30a4218b-dbfd-417b-9c23-2a5e924f85ec');
+		console.log(obj2); // Spline Object => { name: 'Cube', id: '7AF5EBC0-09BB-4720-B045-F478F8053AA4', position: {}, ... }
+
+		//console log all objects and events
+		const allObjects = spline.getAllObjects();
+		console.log('all objects:', allObjects);
+		
+		const splineEvents = spline.getSplineEvents();
+		console.log('all events:', splineEvents);
+
+		// or
+		// const obj = spline.findObjectById('7AF5EBC0-09BB-4720-B045-F478F8053AA4');
+        spline.addEventListener('mouseUp', (e) => {
+			if (e.target.id === '30a4218b-dbfd-417b-9c23-2a5e924f85ec') {
+                console.log('Clicked on Sphere');
+				e.target.rotation.y += Math.PI / 2;
+			}});
+		
+		
+		const zoomSlider = document.getElementById('zoom-slider');
+			//zoomSlider
+		zoomSlider.addEventListener('input', function() {
+			const sliderValue = this.value;
+			const zoomValue = parseFloat(sliderValue);
+			
+			spline.setZoom(zoomValue);
+		});
+
+		const rotateButton = document.getElementById('rotate-button');
+		rotateButton.addEventListener('click', function() {
+		const obj = spline.findObjectByName('Sphere');
+		
+		if (obj) {
+			obj.rotation.y += Math.PI / 2;
+		}
+		});
+
 	});
+
+
+	
+
+
