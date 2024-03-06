b093a5da450adda534443245d936494b70288de5
KennethKcai <97841755+KennethKcai@users.noreply.github.com>
2024-03-06 00:29:20 -0800

    final work

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ const canvas = document.getElementById('canvas3d');
 // start the application and load the scene
 const spline = new Application(canvas);
 let eventToggled = false;
+let eventToggled2 = false;
+let eventToggled3 = false;
+
 
 spline
 	.load('https://prod.spline.design/nTUzA-m9C6a22uwY/scene.splinecode')
@@ -18,10 +21,10 @@ spline
 		const splineEvents = spline.getSplineEvents();
 		console.log('all events:', splineEvents);
 
-
 		//human perspective button
 		const perspectiveButton = document.getElementById('perspective-button');
 		perspectiveButton.addEventListener('click', function() {
+			console.log('clicked')
   		if (eventToggled) {
     		spline.emitEvent('mouseDown', 'Cube 3'); 
     		eventToggled = false;
@@ -31,8 +34,50 @@ spline
     		eventToggled = true;
   		}	
 		});
+		
+		
+  		const sceneButton1 = document.getElementById('scene1');
+  		sceneButton1.addEventListener('click', function() {
+			
+    		if (eventToggled2) {
+      			spline.emitEvent('mouseDown', 'Cube 2'); 
+      			eventToggled2 = false;
+    		}else {
+      			spline.emitEventReverse('mouseDown', 'Cube 2'); 
+      			eventToggled2 = true;
+    		} 
+
+			if (eventToggled3) {
+				spline.emitEvent('mouseDown', 'Cube'); 
+				eventToggled3 = false;
+		  	}else {
+				spline.emitEventReverse('mouseDown', 'Cube'); 
+				eventToggled3 = true;
+		  	} 
+  		});
+
+		const sceneButton2 = document.getElementById('scene2');
+  			sceneButton2.addEventListener('click', function() {
+    		if (eventToggled2) {
+      			spline.emitEvent('mouseDown', 'Cube 2'); 
+      			eventToggled2 = false;
+				eventToggled3 = true;
+    		}else {
+      			spline.emitEventReverse('mouseDown', 'Cube 2'); 
+      			eventToggled2 = true;
+				eventToggled3 = false;
+    		} 
+
+			if (eventToggled3) {
+				spline.emitEvent('mouseDown', 'Cube'); 
+		  	}else {
+				spline.emitEventReverse('mouseDown', 'Cube'); 
+		  	} 
+  		});
+
 
 	});
+
 
 
 	
